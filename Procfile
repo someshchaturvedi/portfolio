3eac539cbc4416b23c2c24b09ed4ab96880ae243
somesh <somesh.08.96@gmail.com>
2016-01-12 22:44:28 +0530

    for hosting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn img.wsgi --log-file -
--- img/wsgi.py
+++ img/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "img.settings")
 
 application = get_wsgi_application()
+
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
